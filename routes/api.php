2cf943509c58fcfcda5804edbea99af836346c8e
gushg <gus.hg2017@gmail.com>
2024-07-18 09:50:01 -0600

    Configuración de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Resources\UserResource;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\AuthController;
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VideoController;
+use App\Http\Controllers\Api\CategoryController;
 
- 
-Route:: get('users/{user}',[UserController::class, "show"])->name('api.users.show');
+Route::get('user/{user}', [UserController::class, 'show'])->name('api.users.show');
 
-Route::get('users',[UserController::class,'index'])->name('api.users.index');
+Route::get('users', [UserController::class,'index'])->name('api.users.index');
 
-Route:: delete('users/{user}',[UserController::class, "destroy"])->name('api.users.delete');
+Route::delete('user/{user}', [UserController::class,'destroy'])->name('api.users.delete');
 
 Route::post('users', [UserController::class, 'store'])->name('api.users.store');
 
@@ -22,10 +24,34 @@ Route::post('register',[AuthController::class,'store'])->name('api.users.store')
 
 Route::post('login',[AuthController::class,'login'])->name('api.users.login');
 
+Route::get('posts', [PostController::class, 'index'])->name('api.posts.index');
 
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('posts/{post}', [PostController::class, 'show'])->name('api.posts.show');
+    Route::post('posts', [PostController::class, 'store'])->name('api.posts.store');
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('api.posts.update');
+    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('api.posts.delete');
+   // Route::get('posts', [PostController::class, 'myPosts'])->name('api.posts.myPosts');
+    Route::get('my-posts', [PostController::class, 'myPosts'])->name('api.posts.myPosts'); 
 });
+//
+
+// Route::get('videos/{video}',[VideoController::class, 'show'])->name('api.videos.show');
+
+// Route::get('videos',[VideoController::class,'index'])->name('api.videos.index');
+
+// Route::post('videos', [VideoController::class, 'store'])->name('api.videos.store');
+
+// Route::put('videos/{video}', [VideoController::class, 'update'])->name('api.videos.update');
+
+// Route::delete('videos/{video}', [VideoController::class, 'destroy'])->name('api.videos.destroy');
+
+//
+
+Route::get('categories/{category}',[CategoryController::class, 'show'])->name('api.categories.show');
+
+Route::get('categories',[CategoryController::class,'index'])->name('api.categories.index');
+
+
+
+
